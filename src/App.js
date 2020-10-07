4ad7df835dbd5581a95b4b264aae5ef856b6a966
lajawanti <lajawanti.dhake@gmail.com>
2020-10-07 17:09:13 -0400

    movies fetching according to provided fetchUrl

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import Row from './components/row/Row.js';
+import requests from './Data/requests.js';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
         <h1>Netflix using RECAT.....</h1>
-        <Row rowTitle = "NETFLIX ORIGINALS"/>
-        <Row rowTitle = "Trending Now"/>
+        <Row rowTitle = "NETFLIX ORIGINALS" fetchUrl = {requests.fetchNetflixOriginals}/>
+        <Row rowTitle = "Trending Now" fetchUrl = {requests.fetchTrending}/>
     </div>
   );
 }
--- src/components/row/Row.js
+++ src/components/row/Row.js
@@ -1,13 +1,38 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from '../../Data/axios.js';
 
-const Row = ({ rowTitle }) => {
+const base_url = 'https://image.tmdb.org/t/p/original/';
+
+const Row = ({ rowTitle, fetchUrl }) => {
       const [movies, setMovies] = useState([]);
 
+      //A snippet of code which runs based on a specific data provided
+      useEffect(() => {
+            async function fetchData() {
+                  const request = await axios.get(fetchUrl);
+                  //console.log(request)
+                  setMovies(request.data.results);
+                  return request;
+            }
+            fetchData();
+
+      }, [fetchUrl]) // [fetchUrl] -- means useEffect is dependent on fetchUrl .. so whenever fetchUrl changes useEffect will run and fetch movies according to fecthUrl provided
+
+      console.table(movies);
+
       return (
-            <div>
+            <div className = "row">
                   {/* rowTitle */}
                   <h2>{rowTitle}</h2>
+
                   {/* container-- movie posters */}
+                  <div className = "row__poster">
+                        {
+                              movies.map((movie, index) => (
+                                    <img src = {`${base_url}${movie.poster_path}`} />
+                              ))
+                        }
+                  </div>
             </div>
       )
 }
