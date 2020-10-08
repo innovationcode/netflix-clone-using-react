d92bb9677cba781c6d88f2688a5a080ab9b33f62
lajawanti <lajawanti.dhake@gmail.com>
2020-10-08 14:56:43 -0400

    row component style

--- src/components/row/Row.js
+++ src/components/row/Row.js
@@ -1,40 +1,43 @@
-import React, { useState, useEffect } from 'react'
-import axios from '../../Data/axios.js';
+import React, { useState, useEffect } from "react";
+import axios from "../../Data/axios.js";
 
-const base_url = 'https://image.tmdb.org/t/p/original/';
+import "./Row.css";
+
+const base_url = "https://image.tmdb.org/t/p/original/";
 
 const Row = ({ rowTitle, fetchUrl }) => {
-      const [movies, setMovies] = useState([]);
+  const [movies, setMovies] = useState([]);
 
-      //A snippet of code which runs based on a specific data provided
-      useEffect(() => {
-            async function fetchData() {
-                  const request = await axios.get(fetchUrl);
-                  //console.log(request)
-                  setMovies(request.data.results);
-                  return request;
-            }
-            fetchData();
+  //A snippet of code which runs based on a specific data provided
+  useEffect(() => {
+    async function fetchData() {
+      const request = await axios.get(fetchUrl);
+      //console.log(request)
+      setMovies(request.data.results);
+      return request;
+    }
+    fetchData();
+  }, [fetchUrl]); // [fetchUrl] -- means useEffect is dependent on fetchUrl .. so whenever fetchUrl changes useEffect will run and fetch movies according to fecthUrl provided
 
-      }, [fetchUrl]) // [fetchUrl] -- means useEffect is dependent on fetchUrl .. so whenever fetchUrl changes useEffect will run and fetch movies according to fecthUrl provided
+  console.table(movies);
 
-      console.table(movies);
+  return (
+    <div className="row">
+      {/* rowTitle */}
+      <h2>{rowTitle}</h2>
 
-      return (
-            <div className = "row">
-                  {/* rowTitle */}
-                  <h2>{rowTitle}</h2>
-
-                  {/* container-- movie posters */}
-                  <div className = "row__poster">
-                        {
-                              movies.map((movie, index) => (
-                                    <img src = {`${base_url}${movie.poster_path}`} />
-                              ))
-                        }
-                  </div>
-            </div>
-      )
-}
+      {/* container-- movie posters */}
+      <div className="row__posters">
+        {movies.map((movie, index) => (
+          <img
+            key={movie.id}
+            className="row__poster"
+            src={`${base_url}${movie.poster_path}`}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default Row;
