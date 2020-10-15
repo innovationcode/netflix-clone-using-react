ebf76e4061a6ac11e67f2243cfa2b462be49f26d
lajawanti <lajawanti.dhake@gmail.com>
2020-10-15 15:29:28 -0400

    Update firebase.js

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,26 +1,14 @@
 import firebase from 'firebase';
 
-
 const firebaseConfig = {
-    apiKey: "AIzaSyCyFMM5SgqhoACPG9Zm_1jECM_nmHfPv_Y",
-    authDomain: "netflix-clone-8e3d5.firebaseapp.com",
-    databaseURL: "https://netflix-clone-8e3d5.firebaseio.com",
-    projectId: "netflix-clone-8e3d5",
-    storageBucket: "netflix-clone-8e3d5.appspot.com",
-    messagingSenderId: "575266645851",
-    appId: "1:575266645851:web:3964bbc252d046347e9c7e"
+    apiKey: process.env.REACT_APP_APIKEY,
+    authDomain: process.env.REACT_APP_AUTHDOMAIN,
+    databaseURL: process.env.REACT_APP_DATABASEURL,
+    projectId: process.env.REACT_APP_PROJECTID,
+    storageBucket: process.env.REACT_APP_STORAGEBUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSINGSENDERID,
+    appId: process.env.REACT_APP_APPID
 };
-
-
-// const firebaseConfig = {
-//     apiKey: process.env.REACT_APP_APIKEY,
-//     authDomain: process.env.REACT_APP_AUTHDOMAIN,
-//     databaseURL: process.env.REACT_APP_DATABASEURL,
-//     projectId: process.env.REACT_APP_PROJECTID,
-//     storageBucket: process.env.REACT_APP_STORAGEBUCKET,
-//     messagingSenderId: process.env.REACT_APP_MESSINGSENDERID,
-//     appId: process.env.REACT_APP_APPID
-// };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 const db = firebaseApp.firestore();
